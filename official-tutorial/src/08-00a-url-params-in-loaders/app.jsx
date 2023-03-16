54cc7ce7762e4313f9d981b1ccaeaa50b51cfd68
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-03-16 07:02:02 -0400

    URL Params in Loaders

---
+++ official-tutorial/src/08-00a-url-params-in-loaders/app.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom"
+
+import "./index.css"
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction
+} from "./routes/root"
+import Contact, {
+  loader as contactLoader
+} from "./routes/contact"
+import ErrorPage from "./error-page"
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      }
+    ]
+  },
+]
+
+// const routes = createRoutesFromElements(
+//   <>
+//     <Route
+//       element={<Root/>}
+//       errorElement={<ErrorPage/>}
+//       path="/"
+//     />
+//     <Route
+//       element={<Contact/>}
+//       path="/contacts/:contactid"
+//     />
+//   </>
+// )
+
+const router = createBrowserRouter(routes)
+
+const App = () => (
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+
+export { App }
