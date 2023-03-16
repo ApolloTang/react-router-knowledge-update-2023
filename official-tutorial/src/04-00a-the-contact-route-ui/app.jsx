2e2be7cbb3547ee0622692f8cd52905b0e52961d
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-03-16 05:38:17 -0400

    added: the contact-route-ui

---
+++ official-tutorial/src/04-00a-the-contact-route-ui/app.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom"
+
+import "./index.css"
+
+import Root from "./routes/root"
+import Contact from "./routes/contact"
+import ErrorPage from "./error-page"
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  }
+]
+
+// const routes = createRoutesFromElements(
+//   <>
+//     <Route
+//       element={<Root/>}
+//       errorElement={<ErrorPage/>}
+//       path="/"
+//     />
+//     <Route
+//       element={<Contact/>}
+//       path="/contacts/:contactid"
+//     />
+//   </>
+// )
+
+const router = createBrowserRouter(routes)
+
+const App = () => (
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+
+export { App }
