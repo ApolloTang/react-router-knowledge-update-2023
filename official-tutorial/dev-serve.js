72e19240480880177b5f878971366f4148f37d30
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-03-18 12:06:47 -0400

    update webpack for devServer.historyApiFallback

--- official-tutorial/dev-serve.js
+++ official-tutorial/dev-serve.js
@@ -41,7 +41,8 @@ function start(port, host) {
     historyApiFallback: true,
       // for historyApiFallback to work
       // must also specified output.publicPath
-      // in webpack.config.js
+      // in webpack.config.js see:
+      //   https://stackoverflow.com/a/53406903/3136861
     hot: true, // Dev server client for web socket transport, hot and live reload logic
     client : {
       logging: 'info',
